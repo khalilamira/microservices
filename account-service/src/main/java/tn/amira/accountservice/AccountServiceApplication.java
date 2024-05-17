c66a2ff9c8ef85d9a3d22b2692e33691d6e39f00
khalilamira <91702854+khalilamira@users.noreply.github.com>
2024-05-17 09:07:31 +0200

    DEV: Test AccountService

--- account-service/src/main/java/tn/amira/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/tn/amira/accountservice/AccountServiceApplication.java
@@ -1,13 +1,29 @@
 package tn.amira.accountservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tn.amira.accountservice.entities.BankAccount;
+import tn.amira.accountservice.repository.BankAccountRepository;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(BankAccountRepository accountRepository){
+        return args -> {
+            BankAccount bankAccount = BankAccount.builder()
+                    .accountId(UUID.randomUUID().toString())
+                    .build();
+
+        };
     }
 
 }
--- account-service/src/main/java/tn/amira/accountservice/web/AccountRestController.java
+++ account-service/src/main/java/tn/amira/accountservice/web/AccountRestController.java
@@ -1,6 +1,7 @@
 package tn.amira.accountservice.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import tn.amira.accountservice.entities.BankAccount;
 import tn.amira.accountservice.repository.BankAccountRepository;
@@ -18,6 +19,11 @@ public class AccountRestController {
     @GetMapping("/accounts")
     public List<BankAccount> accountList(){
         return accountRepository.findAll();
+    }
+
+    @GetMapping("/accounts/{id}")
+    public BankAccount bankAccountById(@PathVariable  String id){
+        return accountRepository.findById(id).get();
     }
 
 
