87ff477467c085a1cb5f0368d39896314657537d
khalilamira <91702854+khalilamira@users.noreply.github.com>
2024-08-07 06:13:52 +0200

    FIX : correction bug recuperation customer by id

--- customer-service/src/main/java/tn/amira/customerservice/web/CustomerRestController.java
+++ customer-service/src/main/java/tn/amira/customerservice/web/CustomerRestController.java
@@ -2,6 +2,7 @@ package tn.amira.customerservice.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tn.amira.customerservice.entities.Customer;
@@ -22,8 +23,8 @@ public class CustomerRestController {
     public List<Customer> customerList(){
         return customerRepository.findAll();
     }
-    @GetMapping("/customer/{id}")
-    public Customer customerById(Long id){
+    @GetMapping("/customers/{id}")
+    public Customer customerById(@PathVariable Long id){
         return customerRepository.findById(id).get();
     }
 }
