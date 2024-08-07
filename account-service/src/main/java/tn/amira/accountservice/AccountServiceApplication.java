a4d89ff60edcef95b14409b85e9fed5524cc5414
khalilamira <91702854+khalilamira@users.noreply.github.com>
2024-08-07 13:30:33 +0200

    DEV: ajout circuitbreaker-resilience4j @circuitBreaker

--- account-service/src/main/java/tn/amira/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/tn/amira/accountservice/AccountServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import tn.amira.accountservice.clients.CustomerRestClient;
 import tn.amira.accountservice.dtos.AccountType;
 import tn.amira.accountservice.entities.BankAccount;
 import tn.amira.accountservice.repository.BankAccountRepository;
@@ -21,29 +22,28 @@ public class AccountServiceApplication {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(BankAccountRepository accountRepository){
+    CommandLineRunner commandLineRunner(BankAccountRepository accountRepository,CustomerRestClient customerRestClient){
         return args -> {
-            BankAccount bankAccount = BankAccount.builder()
-                    .accountId(UUID.randomUUID().toString())
-                    .currency("MAD")
-                    .balance(90000)
-                    .createdAt(LocalDate.now())
-                    .type(AccountType.CURRENT_ACCOUNT)
-                    .customerId(Long.valueOf(1))
-                    .build();
-
-            BankAccount bankAccount2 = BankAccount.builder()
-                    .accountId(UUID.randomUUID().toString())
-                    .currency("MAD")
-                    .balance(120000)
-                    .createdAt(LocalDate.now())
-                    .type(AccountType.CURRENT_ACCOUNT)
-                    .customerId(Long.valueOf(2))
-                    .build();
-
-            accountRepository.save(bankAccount);
-            accountRepository.save(bankAccount2);
-
+            customerRestClient.allCustomers().forEach(c->{
+                BankAccount bankAccount = BankAccount.builder()
+                        .accountId(UUID.randomUUID().toString())
+                        .currency("MAD")
+                        .balance(Math.random()*8000)
+                        .createdAt(LocalDate.now())
+                        .type(AccountType.CURRENT_ACCOUNT)
+                        .customerId(c.getId())
+                        .build();
+                BankAccount bankAccount2 = BankAccount.builder()
+                        .accountId(UUID.randomUUID().toString())
+                        .currency("MAD")
+                        .balance(Math.random()*67399)
+                        .createdAt(LocalDate.now())
+                        .type(AccountType.CURRENT_ACCOUNT)
+                        .customerId(c.getId())
+                        .build();
+                accountRepository.save(bankAccount);
+                accountRepository.save(bankAccount2);
+            });
         };
     }
 
