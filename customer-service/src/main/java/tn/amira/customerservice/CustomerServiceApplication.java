4e8e05b8708b2e50b34212df68b734cfb44e8dc1
khalilamira <91702854+khalilamira@users.noreply.github.com>
2024-08-15 07:14:13 +0200

    DEV: configuration de config-service

--- customer-service/src/main/java/tn/amira/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/tn/amira/customerservice/CustomerServiceApplication.java
@@ -3,13 +3,16 @@ package tn.amira.customerservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import tn.amira.customerservice.config.GlobalConfig;
 import tn.amira.customerservice.entities.Customer;
 import tn.amira.customerservice.repository.CustomerRepository;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GlobalConfig.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
