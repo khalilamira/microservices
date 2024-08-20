d6ece4104522cdf51a4d90afcf425915f1c8a99a
khalilamira <91702854+khalilamira@users.noreply.github.com>
2024-08-20 07:31:29 +0200

    DEV_FRONT: consomation web service get accounts

--- angular-front-end/src/app/accounts/accounts.component.ts
+++ angular-front-end/src/app/accounts/accounts.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-accounts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit{
+  accounts : any ;
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit() {
+    this.http.get("http://localhost:9999/ACCOUNT-SERVICE/accounts")
+      .subscribe({
+          next : data => {
+            this.accounts = data
+          },
+          error : err => {
+            console.log("error");
+          }
+        }
+      )
+  }
 
 }
