f15101673773cad8b1b45421a7a02ed8ae5d0bfb
khalilamira <91702854+khalilamira@users.noreply.github.com>
2024-08-20 07:20:53 +0200

    DEV_FRONT: consommation api custommers httpclient

--- angular-front-end/src/app/customers/customers.component.ts
+++ angular-front-end/src/app/customers/customers.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {CommonModule, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,HttpClientModule , CommonModule
+  ],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit{
+  customers : any;
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit() {
+    this.http.get("http://localhost:9999/CUSTOMER-SERVICE/api/customers")
+      .subscribe({
+        next : data => {
+          this.customers = data;
+        },
+        error : err => {
+          console.log(err);
+        }
+      }
+    )
+  }
 
 }
