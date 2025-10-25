24fc3d53fd8f193575405ca3b5dedbe2fd00ba56
khalilamira <khalilamirapro@gmail.com>
2025-10-25 23:10:17 +0200

    FIX-01 : config

--- customer-service/src/main/java/tn/amira/customerservice/repository/CustomerRepository.java
+++ customer-service/src/main/java/tn/amira/customerservice/repository/CustomerRepository.java
@@ -1,8 +1,9 @@
 package tn.amira.customerservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tn.amira.customerservice.entities.Customer;
 
-
+@RepositoryRestResource
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
 }
