58270ba33e4053ffd81e676b35a56db9e7345074
khalilamira <khalilamirapro@gmail.com>
2025-11-01 07:50:20 +0100

    FIX-01 : lancement service customer todo : comprendre la service de configuration

--- customer-service/src/main/java/tn/amira/customerservice/web/ConfigTestController.java
+++ customer-service/src/main/java/tn/amira/customerservice/web/ConfigTestController.java
@@ -12,21 +12,13 @@ import java.util.Map;
 @RestController
 @RefreshScope
 public class ConfigTestController {
-    @Value("${global.params.p1}")
-    private int p1;
-    @Value("${global.params.p2}")
-    private int p2;
-    @Value("${customer.params.x}")
-    private int x;
-    @Value("${customer.params.y}")
-    private int y;
 
     @Autowired
     private GlobalConfig globalConfig;
 
     @GetMapping("/TestConfig")
     public Map<String,Integer> configTest(){
-        return Map.of("p1",p1,"p2",p2,"x",x,"y",y);
+        return Map.of();
     }
 
     @GetMapping("/globalConfig")
